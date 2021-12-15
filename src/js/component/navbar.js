c8bcd659d4146a68acc863387da3020c1b48ebf0
rcavero <ramoncaveroaras@gmail.com>
2021-12-15 19:39:13 +0000

    después tutoría con Marcos

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 
+import {Context} from "../store/appContext"
+
 export const Navbar = () => {
+
+	const {store} = useContext(Context);
+
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
 				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
 			</Link>
 			<div className="ml-auto">
+			Tareas pendientes: {store.todos.length}
 				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+					<button className="btn btn-primary ms-3">Check the Context in action</button>
 				</Link>
 			</div>
 		</nav>
